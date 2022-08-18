364b4a53983dac82a49112812d58049ee46df7b2
MSI\81804 <a1929776588@gmail.com>
2022-08-18 12:33:01 +0900

    [Add]レイヤーからオブジェクト取得できるようにした

--- Assets/3.Itou/Possession.cs
+++ Assets/3.Itou/Possession.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 public class Possession : MonoBehaviour
 {
-    [SerializeField][Tooltip("当たり判定")] private LayerMask canHitMask;
-    [SerializeField][Tooltip("目線当たったらものを入れる")]GameObject hited;
-    [SerializeField][Tooltip("目線の距離")]private float _rayDistance;
-   
+    [SerializeField][Tooltip("プレイヤーが憑依できる物のレイヤー")] private LayerMask canHitMask;
+    [SerializeField][Tooltip("Rayが当たったらこの中に入れる")] GameObject hited;
+    [SerializeField][Tooltip("Rayの距離")] private float _rayDistance;
+
     RaycastHit hit;
 
     void Update()
     {
-        Debug.DrawRay(gameObject.transform.position, Vector3.forward *_rayDistance, Color.blue, 0.1f);
-        if (Physics.Raycast(gameObject.transform.position, Vector3.forward, out hit, _rayDistance,canHitMask))
+        Debug.DrawRay(gameObject.transform.position, Vector3.forward * _rayDistance, Color.blue, 0.1f);
+        if (Physics.Raycast(gameObject.transform.position, Vector3.forward, out hit, _rayDistance, canHitMask))
         {
             hited = hit.collider.gameObject;
         }
-        else 
+        else
         {
             hited = null;
         }
