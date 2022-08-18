7300a3ed9dc52574df047bc511b5ea001e372d64
MSI\81804 <a1929776588@gmail.com>
2022-08-18 13:54:24 +0900

    [Add]色が変わる関数を作った。

--- Assets/3.Itou/Possession.cs
+++ Assets/3.Itou/Possession.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class Possession : MonoBehaviour
 {
-    [SerializeField][Tooltip("プレイヤーが憑依できる物のレイヤー")] private LayerMask canHitMask;
-    [SerializeField][Tooltip("Rayが当たったらこの中に入れる")] GameObject hited;
+    [SerializeField][Tooltip("プレイヤーが憑依できる物のレイヤー")] private LayerMask _canHitMask;
+    [SerializeField][Tooltip("Rayが当たったらこの中に入れる")] GameObject _hited;
     [SerializeField][Tooltip("Rayの距離")] private float _rayDistance;
+    [SerializeField][Tooltip("Rayが当たったものに変わってほしい色")] Color _color=new Color(210,210,210);
+    MeshRenderer _mr;
 
     RaycastHit hit;
 
     void Update()
     {
         Debug.DrawRay(gameObject.transform.position, Vector3.forward * _rayDistance, Color.blue, 0.1f);
-        if (Physics.Raycast(gameObject.transform.position, Vector3.forward, out hit, _rayDistance, canHitMask))
+        if (Physics.Raycast(gameObject.transform.position, Vector3.forward, out hit, _rayDistance, _canHitMask))
         {
-            hited = hit.collider.gameObject;
+            _hited = hit.collider.gameObject;
+            ColorChange(_hited, _color);
         }
         else
         {
-            hited = null;
+            ColorChange(_hited,Color.white);
+            _hited = null;
         }
+    }
+    public void ColorChange(GameObject gameObject,Color color)
+    {
+        gameObject.GetComponent<MeshRenderer>().material.color = color;
     }
 }
